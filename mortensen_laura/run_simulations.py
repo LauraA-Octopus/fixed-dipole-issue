3c1c0ad4e84fd76d5b1c336371cebb7fa096adb4
LauraA-Octopus <163292135+LauraA-Octopus@users.noreply.github.com>
2025-02-27 14:19:51 +0000

    dinamically run simulations and fit

--- mortensen_laura/run_simulations.py
+++ mortensen_laura/run_simulations.py
@@ -18,7 +18,7 @@ def run_test(phi, theta, filename, num_repeats=3):
             try:
                 # Extract estimated results
                 result_str = result_line[0].split(":")[1].strip()
-                results = result_str #np.fromstring(result_str.strip('[]'), sep=' ').tolist()
+                results = list(map(float,result_str.split(",")))    
                 print(f"Mortensen fitting results are: {results}")
                 
                 # Extract ground truth values
@@ -37,7 +37,7 @@ def main():
     # Define theta and phi values
     chosen_thetas = [0, 22.5, 45, 67.5, 90]
     chosen_phis = [0, 45, 135, 180, 225, 270, 315]
-    fixed_phis = list(range(0, 361, 4))  # [0, 4, 8, ..., 360]
+    fixed_phis = list(range(0, 361, 4))  
     fixed_thetas = list(range(0, 91, 1))
     num_repeats = 3
 
@@ -54,20 +54,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-    
-    # Run tests for fixed theta values with varying phi values
-    #for theta in fixed_thetas:
-    #    filename = f"results_theta_{theta}.csv"
-    #    for i, phi in enumerate(fixed_phis):
-    #        # Change phi after every 'num_copies' (3) iterations
-    #        if i % common_repeats == 0 and i > 0:
-    #            phi += 4  # Increment phi by 4 degrees after every 20 simulations
-    #        run_test(phi * np.pi / 180, theta * np.pi / 180, filename, common_repeats)
-    
-    # Run tests for fixed phi values with varying theta values
-    #for phi in fixed_phis:
-    #    filename = f"results_phi_{phi}.csv"
-    #    for theta in [0, 22.5, 45, 67.5, 90]:
-    #        run_test(phi * np.pi / 180, theta * np.pi / 180, filename, common_repeats)
